0d1551fd4d402d9a88225c90db5d76c353d21bf6
nick3ds <14939913+nick3ds@users.noreply.github.com>
2020-09-30 22:22:42 +0200

    parse settings w/ file

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using System.Windows.Forms;
 using savetray.Properties;
 using System.Diagnostics;
+using System.Collections.Generic;
+using System.IO;
 
 namespace savetray
 {
@@ -18,19 +20,33 @@ namespace savetray
         public class Context : ApplicationContext
         {
             readonly NotifyIcon trayIcon;
+            readonly List<MenuItem> menu = new List<MenuItem>();
+            readonly string settings = @"Resources\settings.txt";
 
             public Context()
             {
                 trayIcon = new NotifyIcon()
                 {
-                    ContextMenu = new ContextMenu(),
                     Icon = Resources.AppIcon,
                     Text = Environment.UserName,
                     Visible = true
                 };
 
-                trayIcon.DoubleClick +=
-                    (sender, e) => Dispatch("notepad", @"Resources\settings.txt");
+                foreach (string line in File.ReadAllLines(settings))
+                {
+                    string[] terms = line.Split(',');
+
+                    if (terms.Length < 3) continue;
+
+                    string label = terms[0];
+                    string path = terms[1];
+                    string args = terms[2];
+
+                    menu.Add(new MenuItem(label, (sender, e) => Dispatch(path, args)));
+                }
+
+                trayIcon.ContextMenu = new ContextMenu(menu.ToArray());
+                trayIcon.DoubleClick += (sender, e) => Dispatch("notepad", settings);
             }
 
             void Dispatch(string file, string args)
