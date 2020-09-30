6a0f2a3a9e0cc0b247615e5800bcc0826c045d27
nick3ds <14939913+nick3ds@users.noreply.github.com>
2020-09-30 19:27:13 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using savetray.Properties;
+
+namespace savetray
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Context());
+        }
+
+        public class Context : ApplicationContext
+        {
+            readonly NotifyIcon trayIcon;
+
+            public Context()
+            {
+                trayIcon = new NotifyIcon()
+                {
+                    ContextMenu = new ContextMenu(),
+                    Icon = Resources.AppIcon,
+                    Text = Environment.UserName,
+                    Visible = true
+                };
+            }
+        }
+    }
+}
